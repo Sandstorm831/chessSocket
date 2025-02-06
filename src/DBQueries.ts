07eb63224724c9b4f8c43ef89411d539909aa17c
Sandstorm831 <gargrohan138@gmail.com>
2025-02-06 14:58:35 +0530

    created db queries

---
+++ src/DBQueries.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient({log: ['query', 'info']});
+
+export async function createGame(black: string, white: string, RoomID: string, PGN: string, result: string){
+    const gameObj = await prisma.game.create({
+        data:{
+            RoomID,
+            white,
+            black,
+            PGN,
+            result
+        }
+    }).then(async () => {
+        await prisma.$disconnect();
+    }).catch(async (e) => {
+        console.log(e);
+        await prisma.$disconnect();
+        process.exit(1);
+    })
+}
